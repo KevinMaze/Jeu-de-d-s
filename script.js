365912d5c10436f5760722cfd0b64adf23e1e50c
Kévin MAZE <kekeproject@hotmail.fr>
2023-05-28 12:50:23 +0200

    remove script and change script2

--- script.js
+++ script.js
@@ -1,76 +1,149 @@
-/*******VARIABLE**********/
-const diceDiv = document.getElementById("diceDiv")
-let activePlayer = 'player 1'
-let globalScore = 0
-let currentScore = 0
+import { Confetti} from './lib/confetti.js'
 
-document.querySelector(".holdDiv").addEventListener("click", () =>{
-  hold()
+/**LES VARIABLES**/
+let namePlayerOne = prompt('Nom du joueur :')
+let namePlayerTwo = prompt('Nom du joueur :')
+document.getElementById("namePlayerOne").innerText = namePlayerOne
+document.getElementById("namePlayerTwo").innerText = namePlayerTwo
+
+let nameOnePara = document.querySelector(".nameOne")
+let nameTwoPara = document.querySelector(".nameTwo")
+let divPlayerOne = document.querySelector(".divPlayerOne")
+let divPlayerTwo = document.querySelector(".divPlayerTwo")
+let winDiv = document.getElementById("win")
+let actifPlayer
+let gameRound = 0
+
+/***CLASSE JOUEUR***/
+class Player {
+  constructor(name, globalScore, currentScore, round, win){
+    this.nam = name
+    this.globalScore = globalScore
+    this.currentScore = currentScore
+    this.round = false
+    this.win = false
+  }
+}
+
+const playerOne = new Player(namePlayerOne, 0, 0, false, false)
+const playerTwo = new Player(namePlayerTwo, 0, 0, false, false)
+const players = [playerOne, playerTwo]
+
+
+/****AJOUT EVENEMENTS****/
+document.getElementById("newGame").addEventListener("click", () => {
+  newGame()
 })
-document.querySelector(".rollDiceDiv").addEventListener("click", () =>{
+
+document.querySelector('.rollDiceDiv').addEventListener("click", () =>{
   rollDice()
 })
 
-/*FONCTION GENERATION ALEA*/
+document.querySelector(".holdDiv").addEventListener("click", () => {
+  hold()
+})
+
+/*****FONCTION NEW GAME*******/
+function newGame(){
+  Confetti.stopAnimationConfeti()
+  winDiv.innerText = ''
+  gameRound = 0
+  playerOne.globalScore = '0'
+  playerTwo.globalScore = '0'
+}
+
+/********FONCTION ROLL DICE (nb aléa et changement de joueur********/
 function rollDice(){
+  winDiv.innerText = ''
   const randomDice = Math.floor(Math.random() * 6 + 1)
   const result = randomDice
   console.log(result)
   const imgDice = document.querySelector("#imgDice")
   imgDice.src = './images/dice' + result +'.png'
-  /*****AJOUT POINT AU CURRENT SCORE****/
-  if(result > 1){
-    currentScore + result
-    console.log('continu à jouer')
+  /****VOIR LE NB DE TOUR****/
+  if(gameRound == 0){
+    players[0].tour = true
+    actifPlayer = players[0]
+    actifPlayer.currentScore += result
+    currentScoreDivs()
+    changeCssPlayer()
   }
-  else{
-    switchPlayer()
+  /*****VOIR SI RANDOM RESULT = 1 ET CHANGMENT DE JOUEUR AVEC REVERSE () (inverse ordre tableau)****/
+  if(result == 1){
+    winDiv.innerText = 'Perdu, joueur suivant !'
+    setTimeout (() =>{
+      changeCssPlayer()
+      players[0].currentScore = 0
+      actifPlayer.tour = false
+      players.reverse()
+      players[0].tour = true
+      currentScoreDivs()
+    }, 2000)
   }
 }
 
 
-/*********CHANGEMENT DE JOUEUR********/
-function switchPlayer() {
-  if (activePlayer === 'player 1') {
-    activePlayer = 'player 2'
-  } else {
-    activePlayer = 'player 1'
-  }
-  //changer la variable par joueur
-  switch (activePlayer) {
-    case 'player 1': {
-      console.log("Joueur 1")
-      const currentScorePlayerOne = document.getElementById("currentScorePlayerOne")
-      const globalScorePlayerOne = document.getElementById("globalScorePlayerOne")
-      currentScorePlayerOne.textContent = currentScore
-      globalScorePlayerOne.textContent = globalScore
-      break;
-    }
-    case 'player 2': {
-      console.log("Joueur 2")
-      const currentScorePlayerTwo = document.getElementById("currentScorePlayerTwo")
-      const globalScorePlayerTwo = document.getElementById("globalScorePlayerTwo")
-      currentScorePlayerTwo.textContent = currentScore
-      globalScorePlayerTwo.textContent = globalScore
-      break
-    }
-    default: {
-      console.log('erreur de sélection de joueur');
-    }
-  } 
-}
-
-/*******FONCTION HOLD*********/
+/********FONCTION HOLD (récupérer les points du current jusqu'au global et changer de joueur)*****/
 function hold(){
-  globalScore += currentScore
-  globalScore.textContent = globalScore
-  current = 0
-  if(globalScore >= 100){
-    diceDiv.innerHTML = `${activePlayer} win`
+  players[0].globalScore += players[0].currentScore
+  players[0].currentScore = 0
+  players[0].tour = false
+  if(players[0].globalScore >= 10){
+    players[0].win = true
+    Confetti.launchAnimationConfeti()
+    if(players[0] == playerOne){
+      winDiv.textContent = `Joueur ${namePlayerOne} gagne !`
+    }
+    if(players[0] == playerTwo){
+      winDiv.textContent = `Joueur ${namePlayerTwo} gagne !`
+    }
   }
-  switchPlayer()
-  currentScorePlayerOne = document.getElementById("currentScorePlayerOne").textContent = 0
-  currentScorePlayerTwo = document.getElementById("currentScorePlayerTwo").textContent = 0
+  setTimeout(() => {
+    currentScoreDivs()
+    players.reverse()
+    players[0].tour = true
+    globalScoreHtml()
+    changeCssPlayer()
 
+  }, 1500)
 }
 
+/*****SCORE EN CURRENT EN HTML******/
+function currentScoreDivs(){
+  if(actifPlayer = playerOne){
+    document.getElementById("currentScorePlayerOne").textContent = actifPlayer.currentScore
+  }
+  if(actifPlayer = playerTwo){
+    document.getElementById("currentScorePlayerTwo").textContent = actifPlayer.currentScore
+  }
+}
+
+/******SCORE EN GLOBAL EN HTML********/
+function globalScoreHtml(){
+  if(actifPlayer = playerOne){
+    document.getElementById("globalScorePlayerOne").textContent = actifPlayer.globalScore
+  }
+  if(actifPlayer = playerTwo){
+    document.getElementById("globalScorePlayerTwo").textContent = actifPlayer.globalScore
+  }
+}
+
+/******CHANGEMENT STYLE EN FONCTION DU JOUEUR******/
+function changeCssPlayer(){
+  if(players[0] == playerOne){
+    divPlayerOne.style.backgroundColor = "rgb(195, 195, 195)"
+    divPlayerTwo.style.backgroundColor = "white"
+    nameOnePara.style.textDecoration = "underline"
+    nameTwoPara.style.textDecoration = "none"
+    nameOnePara.style.color = "green"
+    nameTwoPara.style.color = "black"
+  }
+  if(players[0] == playerTwo){
+    divPlayerTwo.style.backgroundColor = "rgb(195, 195, 195)"
+    divPlayerOne.style.backgroundColor = "white"
+    nameOnePara.style.textDecoration = "none"
+    nameTwoPara.style.textDecoration = "underline"
+    nameTwoPara.style.color = "green"
+    nameOnePara.style.color = "black"
+  }
+}
